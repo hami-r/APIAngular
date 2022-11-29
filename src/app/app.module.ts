04ff6ffcd7e1f283bcce3e5cf9cf315bbe91bf39
hami-r <mynameissaitama54@gmail.com>
2022-11-29 09:45:34 +0530

    view webtools added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebtoolsComponent } from './webtools/webtools.component';
+
+let myRoutes:Routes = [
+  {
+    path:"",
+    component:WebtoolsComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebtoolsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
